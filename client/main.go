b2b02663b7ab31f056dfe960f68af3092437b481
fallra1n <tozhimukhammedov.a@gmail.com>
2026-04-11 12:57:05 +0300

    wip

--- client/main.go
+++ client/main.go
@@ -1,1 +1,38 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/fallra1n/demo/proto/gen/go/ping"
+)
+
+func main() {
+	conn, err := grpc.NewClient(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться: %v", err)
+	}
+	defer conn.Close()
+
+	client := ping.NewPingClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.Ping(ctx, &ping.Request{
+		Message: "ping",
+	})
+	if err != nil {
+		log.Fatalf("Ошибка при вызове метода: %v", err)
+	}
+
+	fmt.Printf("Ответ от сервера: %s\n", resp.GetMessage())
+}
